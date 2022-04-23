14b647b6e8cf84c64c52d99824ed3cd390497f9d
maanoel <manoelvitorbrito@gmail.com>
2022-04-23 19:36:59 -0300

    Init

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -20,5 +20,33 @@ namespace TestProject
 
       Assert.True(testedValue.SequenceEqual(difference));
     }
+
+    [Fact]
+    public void Should_Contain_Only_Three_Elements()
+    {
+      SymmetricDifferenceCalculator calculator = new SymmetricDifferenceCalculator();
+
+      int[] valueOne = new int[] { 1, 2, 3 };
+      int[] valueTwo = new int[] { 5, 2, 1, 4 };
+
+      var difference = calculator.Sym(valueOne, valueTwo);
+      int testedValue = difference.Count();
+
+      Assert.True(testedValue == 3);
+    }
+
+    [Fact]
+    public void Should_Return_345_When()
+    {
+      SymmetricDifferenceCalculator calculator = new SymmetricDifferenceCalculator();
+
+      int[] valueOne = new int[] { 1, 2, 3, 3 };
+      int[] valueTwo = new int[] { 5, 2, 1, 4 };
+
+      var difference = calculator.Sym(valueOne, valueTwo);
+      int[] testedValue = new int[] { 3, 4, 5 };
+
+      Assert.True(testedValue.SequenceEqual(difference));
+    }
   }
 }
